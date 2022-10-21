b36f27549eb18c09a8467440aa558c11c2a8640f
thomasbetend <thomas.betend@yahoo.fr>
2022-10-21 12:22:00 +0200

    styling a bit with bootstrap

--- form.php
+++ form.php
@@ -1,33 +1,49 @@
-<form action="/thanks.php" method="post">
-    <div>
-        <label for="lastname">Nom</label>
-        <input type="text" id="lastname" name="user_lastname">
-    </div>
-    <div>
-        <label for="firstname">Prénom</label>
-        <input type="text" id="firstname" name="user_firstname">
-    </div>
-    <div>
-        <label for="email">Email</label>
-        <input type="email" id="email" name="user_email">
-    </div>
-    <div>
-        <label for="phone">Téléphone</label>
-        <input type="tel" id="phone" name="user_phone">
-    </div>
-    <div>
-        <label for="subject">Sujet</label>
-        <select id="subject" name="user_subject">
-            <option value="friendly">Message amical</option>
-            <option value="claim">Message de réclamation</option>
-            <option value="info">Message à caractère informatif</option>
-        </select>
-    </div>
-    <div>
-        <label for="message">Message</label>
-        <textarea id="message" name="user_message" ></textarea>
-    </div>
-    <div>
-        <button type="submit">Envoyer le message</button>
-    </div>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+            rel="stylesheet">
+    <title>Formulaire</title>
+</head>
+<body>
+    
+</body>
+</html>
+<div class="container w-50">
+    <form action="/thanks.php" method="post" class="mt-3">
+        <div class="form-group mb-2">
+            <label for="firstname">Prénom</label>
+            <input type="text" id="firstname" name="user_firstname" class="form-control">
+        </div>
+        <div class="form-group mb-2">
+            <label for="lastname">Nom</label>
+            <input type="text" id="lastname" name="user_lastname" class="form-control">
+        </div>
+        <div class="form-group mb-2">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="user_email" class="form-control">
+        </div>
+        <div class="form-group mb-2">
+            <label for="phone">Téléphone</label>
+            <input type="tel" id="phone" name="user_phone" class="form-control">
+        </div>
+        <div class="form-group mb-2">
+            <label for="subject">Sujet</label>
+            <select id="subject" name="user_subject" class="form-control">
+                <option value="GOOD VIBES">Message amical</option>
+                <option value="BAD VIBES">Message de réclamation</option>
+                <option value="INFO?">Message à caractère informatif</option>
+            </select>
+        </div>
+        <div class="form-group mb-2">
+            <label for="message">Message</label>
+            <textarea id="message" name="user_message" class="form-control"></textarea>
+        </div>
+        <div>
+            <button type="submit" class="btn btn-primary mt-2">Envoyer le message</button>
+        </div>
+    </form>
+</div>
--- thanks.php
+++ thanks.php
@@ -1,6 +1,21 @@
-<h2>
-    Merci <?php echo $_POST['user_lastname'] . ' ' . $_POST['user_firstname'] ?> de nous avoir contacté à propos de <?php echo $_POST['user_subject'] ?>.<br/>
-    Un de nos conseiller vous contactera soit à l’adresse <?php echo $_POST['user_email'] ?> <br/>soit par téléphone au <?php echo $_POST['user_phone'] ?> 
-    dans les plus brefs délais pour traiter votre demande : <br/>
-    <span style="text-transform: uppercase; color: red;"><?php echo $_POST['user_message']?></span>
-</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+            rel="stylesheet">
+    <title>Message validé</title>
+</head>
+<body>
+    <div class="container w-50">
+        <div class="p-3 mt-5">
+            Merci <span class="text-primary"><?php echo ucfirst($_POST['user_firstname']) . ' ' . ucfirst($_POST['user_lastname']) ?></span> de nous avoir contacté à propos de <span class="text-primary"><?php echo $_POST['user_subject'] ?></span>.<br/>
+            Un de nos conseiller vous contactera soit à l’adresse <span class="text-primary"><?php echo $_POST['user_email'] ?></span> soit par téléphone au <span class="text-primary"><?php echo $_POST['user_phone'] ?></span>
+            dans les plus brefs délais pour traiter votre demande : <br/>
+            <em class="text-secondary"> "<?php echo $_POST['user_message']?>"</em>
+        </div>
+    </div>
+</body>
+</html>
